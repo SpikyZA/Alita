399f2f91c3d2829bbd76741890464b7c4c8bc242
Michael Ross Tarr <mrmrmtarr@gmail.com>
2020-11-22 12:46:06 +0200

    Added Logschannel to guild schematics

--- database/Schematics/Guild.js
+++ database/Schematics/Guild.js
@@ -6,5 +6,6 @@ module.exports = mongoose.model("Guild", new mongoose.Schema({
   id: { type: String }, //ID of the guild
   registeredAt: { type: Number, default: Date.now() },
   prefix: { type: String, default: config.prefix },
+  logChannel: ( type: String, default: NULL ),
 
 }));
